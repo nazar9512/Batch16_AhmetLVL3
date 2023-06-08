db46d142f12ba81d9fba24974b505d02ab39fafa
Nazar <nazarkoo1995@gmail.com>
2023-06-08 16:01:20 -0500

    OpenChart project added

--- src/test/java/com/test/bank/tests/BankLoginTest.java
+++ src/test/java/com/test/bank/tests/BankLoginTest.java
@@ -9,17 +9,11 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class BankLogicTest {
+public class BankLogicTest extends BankTestBase{
 
     @Test
     public void validatePageComponents(){
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.navigate().to("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+
 
         BankLoginPage bankLoginPage = new BankLoginPage(driver);
         bankLoginPage.LoginPageComponentValidation("XYZ Bank");
